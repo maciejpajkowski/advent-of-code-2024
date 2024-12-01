b0a8766dd0cd08023a48e0d319542f3562d95dc9
Maciek <huntpro@gmail.com>
2024-12-01 13:40:24 +0100

    day1: doesn't work yet, endpoint returns 400

--- Day1.cs
+++ Day1.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Net.Http.Headers;
+
+using var client = new HttpClient();
+
+try
+{
+    using HttpResponseMessage response = await client.GetAsync("https://adventofcode.com/2024/day/1/input");
+    response.EnsureSuccessStatusCode();
+    string responseBody = await response.Content.ReadAsStringAsync();
+
+}
+catch (HttpRequestException e)
+{
+    Console.WriteLine($"Something broke: {e}");
+}
