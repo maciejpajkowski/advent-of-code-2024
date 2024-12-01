7dba747b32e3494244ccac5dc585ac5f76e6c753
Maciek <huntpro@gmail.com>
2024-12-01 20:52:45 +0100

    day1A: added cookie

--- Day1A.cs
+++ Day1A.cs
@@ -4,9 +4,11 @@ using var client = new HttpClient();
 
 try
 {
+    client.DefaultRequestHeaders.Add("cookie", "session=53616c7465645f5fa0a6a05092e4eb5e115902dc57e7cfc11d56a6f7d756470203b4c4e9fb8defaf0fd1ad8bba5f6e73391b405f4033556ec9f8f0e5cae41750");
     using HttpResponseMessage response = await client.GetAsync("https://adventofcode.com/2024/day/1/input");
     response.EnsureSuccessStatusCode();
     string responseBody = await response.Content.ReadAsStringAsync();
+
 
 }
 catch (HttpRequestException e)
