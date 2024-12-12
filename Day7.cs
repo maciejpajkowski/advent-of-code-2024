473516c45176e3e414e90fcc093eaacc6b667598
Maciek <huntpro@gmail.com>
2024-12-12 22:03:56 +0100

    day7: part one wip

--- Day7.cs
+++ Day7.cs
@@ -1,31 +1,97 @@
+using System.Text.RegularExpressions;
+
 namespace Advent;
 
 public class Day7
 {
+    private static long totalResult = 0;
+
     public static async Task Solve()
     {
-        string responseBody = @"190: 10 19
+        string responseBody = @"1040: 2 1 8 13 65 5
+489775464841: 754 909 829 862 9 4
+190: 10 19
 3267: 81 40 27
 83: 17 5
 156: 15 6
-7290: 6 8 6 15
+7290: 6 8 6 15 // 1 + + + 2 * * * 3 * + + 4 * * + 5 + * + 6 + * * 7 * + * 8 + + *
 161011: 16 10 13
 192: 17 8 14
 21037: 9 7 18 13
 292: 11 6 16 20"; //await AdventClient.GetDataForDay(7);
         List<string> lines = responseBody.Split("\n").ToList(); //.SkipLast(1).ToList();
-        List<long> results = [];
+        List<int> results = [];
 
         foreach (var line in lines)
         {
-            results.Add(GetExpectedResult(line));
+            if (IsCalibrationCorrect(GetExpectedResult(line), GetTestNumbers(line)))
+            {
+                totalResult += GetExpectedResult(line);
+            }
         }
+
+        Console.WriteLine(totalResult);
     }
 
-    private static long GetExpectedResult(string line)
+    private static int GetExpectedResult(string line)
     {
-        string[] splitString = line.Split(":");
+        return Convert.ToInt32(line.Split(":")[0]);
+    }
 
-        return Convert.ToInt32(splitString[0]);
+    private static List<int> GetTestNumbers(string line)
+    {
+        return line.Split(" ").Skip(1).Select((item) => Convert.ToInt32(item)).ToList();
+    }
+
+    private static List<string> GetAllPossibleEquations(List<int> testNumbers, int index)
+    {
+        List<string> equations = [];
+
+        if (index == testNumbers.Count - 1)
+        {
+            equations.Add(testNumbers[index].ToString());
+            return equations;
+        }
+        List<string> subCombinations = GetAllPossibleEquations(testNumbers, index + 1);
+
+        foreach (var subCombination in subCombinations)
+        {
+            foreach (var op in new List<string> { "+", "*" })
+            {
+                equations.Add(testNumbers[index] + op + subCombination);
+            }
+        }
+
+        return equations;
+    }
+
+    private static List<string> ExtractOperations(List<string> equations)
+    {
+        List<string> operations = [];
+
+        foreach (var equation in equations)
+        {
+            MatchCollection matches = Regex.Matches(equation, @"[^\d]");
+
+            string equationOperations = string.Empty;
+
+            foreach (Match match in matches)
+            {
+                equationOperations += match.Value;
+            }
+
+            operations.Add(equationOperations);
+        }
+
+        return operations;
+    }
+
+    private static bool IsCalibrationCorrect(int expectedResult, List<int> testNumbers)
+    {
+        List<char> testedScenarios = [];
+        var possibleOperations = ExtractOperations(GetAllPossibleEquations(testNumbers, 0));
+        bool wasCorrectCombinationFound = false;
+
+        return false;
     }
 }
